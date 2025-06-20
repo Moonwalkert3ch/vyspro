adc5ee79deda0aa3730b9d245a42cf53f02681cd
Moonwalkert3ch <6620@holbertonstudents.com>
2025-06-20 01:05:03 -0500

    removed db files for now placed in safe environment. vercel stopped deploying

--- vys-pro/app/api/vysdb/route.ts
+++
@@ -1,7 +0,0 @@
-// app/api/some-route/route.ts
-import pool from '@/lib/db';
-
-export async function GET() {
-  const result = await pool.query('SELECT * FROM messages');
-  return Response.json(result.rows);
-}
